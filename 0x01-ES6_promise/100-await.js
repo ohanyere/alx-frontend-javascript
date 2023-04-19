4fb5f5f320baf6e2d802fdc0c34d57114d425f5d
ohanyere <mooref068@gmail.com>
2023-04-19 21:09:25 +0000

    advanced task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+module.exports = asyncUploadUser;
