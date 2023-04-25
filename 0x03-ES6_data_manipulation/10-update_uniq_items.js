b91c9ebc7bb71a80423704a300aa67ecf054189f
ohanyere <mooref068@gmail.com>
2023-04-25 21:00:11 +0000

    advanced task

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of groceriesMap.entries()) {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  }
+
+  return groceriesMap;
+}
+// module.exports = {
+//  updateUniqueItems
+// };
