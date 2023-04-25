2442d70a1b938e49481e0a6644536f64fb64f705
ohanyere <mooref068@gmail.com>
2023-04-25 21:01:10 +0000

    advanced task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
+
+module.exports = { weakMap, queryAPI };
